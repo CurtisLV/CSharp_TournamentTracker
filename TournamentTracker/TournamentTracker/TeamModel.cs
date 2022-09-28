d128ead8da57893fefc704833969083001758f0d
CurtisLV <curtislv@gmail.com>
2022-09-28 21:24:27 +0300

    Class Library creation

---
+++ TournamentTracker/TournamentTracker/TeamModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary;
+
+internal class TeamModel
+{
+    public List<Person> TeamMembers {
+        get; set;
+    }
+    public string TeamName {
+        get; set;
+    }
+}
