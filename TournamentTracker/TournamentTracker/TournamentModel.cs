146e65e711e6b1012269d6fbf6e4005c02ee7590
CurtisLV <curtislv@gmail.com>
2022-09-28 22:03:14 +0300

    Csharpier save

--- TournamentTracker/TournamentTracker/TournamentModel.cs
+++ TournamentTracker/TournamentTracker/TournamentModel.cs
@@ -8,20 +8,9 @@ namespace TrackerLibrary;
 
 internal class TournamentModel
 {
-    public string TournamentName {
-        get; set;
-    }
-    public decimal EntryFee {
-        get; set;
-    }
-    public List<TeamModel> EnteredTeams {
-        get; set;
-    } = new List<TeamModel>();
-    public List<PrizeModel> Prizes {
-        get; set;
-    } = new List<PrizeModel>();
-    public List<List<MatchupModel>> Rounds {
-        get; set;
-    } = new List<List<MatchupModel>>();
-
+    public string TournamentName { get; set; }
+    public decimal EntryFee { get; set; }
+    public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+    public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+    public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
 }
