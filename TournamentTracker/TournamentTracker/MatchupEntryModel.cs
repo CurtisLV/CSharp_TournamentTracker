6dc4e45362efdf46f7b5d726af5389693e5fdf8c
CurtisLV <curtislv@gmail.com>
2022-09-28 22:20:56 +0300

    MatchupEntryModel class init

--- TournamentTracker/TournamentTracker/MatchupEntryModel.cs
+++ TournamentTracker/TournamentTracker/MatchupEntryModel.cs
@@ -8,7 +8,19 @@ namespace TrackerLibrary;
 
 internal class MatchupEntryModel
 {
+    /// <summary>
+    /// Represents one team in the matchup
+    /// </summary>
     public TeamModel TeamCompeting { get; set; }
+
+    /// <summary>
+    /// Represents score for this particular team
+    /// </summary>
     public double Score { get; set; }
+
+    /// <summary>
+    /// Represents the matchup that his team
+    /// came from as the winner
+    /// </summary>
     public MatchupModel ParentMatchup { get; set; }
 }
