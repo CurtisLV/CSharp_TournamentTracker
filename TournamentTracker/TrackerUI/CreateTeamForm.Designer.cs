aa8b6965434b287574745b713dce8c38de1356b9
CurtisLV <curtislv@gmail.com>
2022-09-30 20:08:21 +0300

    20:08 lost shit again, but now learned to fix lost forms

--- TournamentTracker/TrackerUI/CreateTeamForm.Designer.cs
+++ TournamentTracker/TrackerUI/CreateTeamForm.Designer.cs
@@ -105,7 +105,7 @@
             this.groupBox1.Size = new System.Drawing.Size(384, 297);
             this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Add New Member";
             // 
             // CreateTeamForm
             // 
