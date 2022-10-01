73f031f5d268a96726490bab9b3e8320af431883
CurtisLV <curtislv@gmail.com>
2022-10-01 22:59:57 +0300

    SqlConnector class 1

--- TournamentTracker/TournamentTracker/SqlConnector.cs
+++ TournamentTracker/TournamentTracker/SqlConnector.cs
@@ -8,9 +8,15 @@ namespace TrackerLibrary;
 
 internal class SqlConnector : IDataConnection
 {
-    //
+    // TODO - Make the CreatePrize method actually save to the database
+    /// <summary>
+    /// Saves a new prize to the database
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns> The prize information, including the unique identifier </returns>
     public PrizeModel CreatePrize(PrizeModel model)
     {
-        //
+        model.Id = 1;
+        return model;
     }
 }
