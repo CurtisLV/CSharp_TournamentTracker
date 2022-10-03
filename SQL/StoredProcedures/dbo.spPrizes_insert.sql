c98580212cc6560c1decb7048f91cba781116147
CurtisLV <curtislv@gmail.com>
2022-10-03 21:18:42 +0300

    Adding Stored procedures

---
+++ SQL/StoredProcedures/dbo.spPrizes_insert.sql
@@ -0,0 +1,31 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE dbo.spPrizes_Insert
+	-- Add the parameters for the stored procedure here
+	@PlaceNumber int,
+	@PlaceName nvarchar(50),
+	@PrizeAmount money,
+	@PrizePercentage float,
+	@id int = 0 output -- unique id will not be written to DB but sent back 
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	insert into dbo.Prizes (PlaceNumber, PlaceName, PrizeAmount, PrizePercentage)
+	values (@PlaceNumber, @PlaceNumber, @PrizeAmount, @PrizePercentage);
+
+	select @id = SCOPE_IDENTITY(); -- last identity created
+END
+GO
