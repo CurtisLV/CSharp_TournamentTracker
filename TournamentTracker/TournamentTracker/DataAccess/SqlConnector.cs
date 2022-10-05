41573d81e64ea81527ff4b29e13a8ed2aa0a5f63
CurtisLV <curtislv@gmail.com>
2022-10-05 20:20:39 +0300

    SQL Connection - function to items to db 2

--- TournamentTracker/TournamentTracker/DataAccess/SqlConnector.cs
+++ TournamentTracker/TournamentTracker/DataAccess/SqlConnector.cs
@@ -34,6 +34,11 @@ public class SqlConnector : IDataConnection
             p.Add("@PrizeAmount", model.PrizeAmount);
             p.Add("@PrizePercentage", model.PrizePercentage);
             p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+            // executes stored procedure with above .add() as parametres
+            connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+            model.Id = p.Get<int>("@id");
         }
     }
 }
