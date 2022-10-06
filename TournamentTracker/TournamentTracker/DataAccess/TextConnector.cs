bf86b9c2c438bf60bf72ccfe5d3c20782082b34f
CurtisLV <curtislv@gmail.com>
2022-10-06 18:38:57 +0300

    Init

--- TournamentTracker/TournamentTracker/DataAccess/TextConnector.cs
+++ TournamentTracker/TournamentTracker/DataAccess/TextConnector.cs
@@ -7,8 +7,6 @@ public class TextConnector : IDataConnection
     // TODO - Wire up the CreatePrize for text files
     public PrizeModel CreatePrize(PrizeModel model)
     {
-        model.Id = 1;
-
-        return model;
+        //
     }
 }
