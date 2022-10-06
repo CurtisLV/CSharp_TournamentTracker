0521c312ae91ff3972e32370b4456a62f6174651
CurtisLV <curtislv@gmail.com>
2022-10-06 18:49:54 +0300

    Move generic methods to new class

--- TournamentTracker/TournamentTracker/DataAccess/TextConnector.cs
+++ TournamentTracker/TournamentTracker/DataAccess/TextConnector.cs
@@ -7,6 +7,16 @@ public class TextConnector : IDataConnection
     // TODO - Wire up the CreatePrize for text files
     public PrizeModel CreatePrize(PrizeModel model)
     {
-        //
+        // Load the text file
+
+        // Convert the text to List<PrizeModel>
+
+        // Find the max ID (like last row)
+
+        // Add new record with the new ID
+
+        // Convert prizes to List<string>
+
+        // Save the List<string> to the text file
     }
 }
---
+++ TournamentTracker/TournamentTracker/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,6 @@
+﻿namespace TrackerLibrary.DataAccess;
+
+internal class TextConnectorProcessor
+{
+    //
+}
