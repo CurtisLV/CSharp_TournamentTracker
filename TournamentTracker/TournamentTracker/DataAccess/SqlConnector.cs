80e0a12e54336e9524a2a28a9519b1e1da86f2ae
CurtisLV <curtislv@gmail.com>
2022-10-07 20:23:23 +0300

    Adding PersonModel to SQL connection

--- TournamentTracker/TournamentTracker/DataAccess/SqlConnector.cs
+++ TournamentTracker/TournamentTracker/DataAccess/SqlConnector.cs
@@ -8,7 +8,27 @@ public class SqlConnector : IDataConnection
 {
     public PersonModel CreatePerson(PersonModel model)
     {
-        throw new NotImplementedException();
+        // Using statement makes sure that connection is closed at the end of curly brace
+        using (
+            IDbConnection connection = new System.Data.SqlClient.SqlConnection(
+                GlobalConfig.ConnectionString("Tournaments")
+            )
+        )
+        {
+            var p = new DynamicParameters();
+            p.Add("@FirstName", model.FirstName);
+            p.Add("@LastName", model.LastName);
+            p.Add("@EmailAddress", model.EmailAddress);
+            p.Add("@CellPhoneNumber", model.PhoneNumber);
+            p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+            // executes stored procedure with above .add() as parametres
+            connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+            model.Id = p.Get<int>("@id");
+
+            return model;
+        }
     }
 
     // TODO - Make the CreatePrize method actually save to the database
