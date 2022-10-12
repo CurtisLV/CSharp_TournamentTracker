e296e2c0c3f16fccef28e99660ea0eb70a78c5e8
CurtisLV <curtislv@gmail.com>
2022-10-12 20:03:54 +0300

    2 Stored procedures

---
+++ SQL/StoredProcedures/dbo.spTeams_Insert.sql
@@ -0,0 +1,18 @@
+
+CREATE PROCEDURE dbo.spTeams_Insert 
+	@TeamName nvarchar(100),
+	@id int = 0 output
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+
+	insert into dbo.Teams (TeamName)
+	values (@TeamName)
+
+	select @id = SCOPE_IDENTITY();
+END
+GO
