bf147364dd054597f1082f9d8b07a15ad6d45a58
CurtisLV <curtislv@gmail.com>
2022-10-14 19:42:06 +0300

    19:42

--- TournamentTracker/TrackerUI/CreateTournamentForm.cs
+++ TournamentTracker/TrackerUI/CreateTournamentForm.cs
@@ -1,14 +1,21 @@
-﻿using TrackerLibrary.Models;
+﻿using TrackerLibrary;
+using TrackerLibrary.Models;
 
 namespace TrackerUI
 {
     public partial class CreateTournamentForm : Form
     {
-        List<TeamModel> availableTeams = new List<TeamModel>();
+        List<TeamModel> availableTeams = GlobalConfig.Connection.GetTeam_All();
 
         public CreateTournamentForm()
         {
             InitializeComponent();
+        }
+
+        private void InitializeLists()
+        {
+            selectTeamDropDown.DataSource = availableTeams;
+            selectTeamDropDown.DisplayMember = "TeamName";
         }
     }
 }
