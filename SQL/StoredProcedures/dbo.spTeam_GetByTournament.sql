7139c1e9f8ff4bb19182490de25c1c4e8a97e0e3
CurtisLV <curtislv@gmail.com>
2022-10-14 18:59:52 +0300

    sp new

---
+++ SQL/StoredProcedures/dbo.spTeam_GetByTournament.sql
@@ -0,0 +1,23 @@
+USE [Tournaments]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE dbo.spTeam_GetByTournament
+	@TournamentId int
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	select t.* 
+	from dbo.Teams t
+	inner join dbo.TournamentEntries e on t.id = e.TeamID
+	where e.TournamentID = @TournamentId;
+END
+GO
