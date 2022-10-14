d7dcee42a206029a4f39b9b84f13c9cb1056dc00
CurtisLV <curtislv@gmail.com>
2022-10-14 20:12:28 +0300

    WireUpLists()

--- TournamentTracker/TrackerUI/CreateTournamentForm.cs
+++ TournamentTracker/TrackerUI/CreateTournamentForm.cs
@@ -7,20 +7,49 @@ namespace TrackerUI
     {
         List<TeamModel> availableTeams = GlobalConfig.Connection.GetTeam_All();
         List<TeamModel> selectedTeams = new List<TeamModel>();
+        List<PrizeModel> selectedPrizes = new List<PrizeModel>();
 
         public CreateTournamentForm()
         {
             InitializeComponent();
-            InitializeLists();
+            WireUpLists();
         }
 
-        private void InitializeLists()
+        private void WireUpLists()
         {
+            selectTeamDropDown.DataSource = null;
             selectTeamDropDown.DataSource = availableTeams;
             selectTeamDropDown.DisplayMember = "TeamName";
 
+            tournamentTeamsListBox.DataSource = null;
             tournamentTeamsListBox.DataSource = selectedTeams;
             tournamentTeamsListBox.DisplayMember = "TeamName";
+
+            prizesListBox.DataSource = null ;
+            prizesListBox.DataSource = selectedPrizes;
+            prizesListBox.DisplayMember = "PlaceName";
+        }
+
+        private void addTeamButton_Click(object sender, EventArgs e)
+        {
+            //PersonModel p = (PersonModel)selectTeamMemberDropDown.SelectedItem;
+
+            //if (p != null)
+            //{
+            //    availableTeamMembers.Remove(p);
+            //    selectedTeamMembers.Add(p);
+
+            //    WireUpLists();
+            //}
+
+            TeamModel team = (TeamModel)selectTeamDropDown.SelectedItem;
+            if (team != null)
+            {
+                availableTeams.Remove(team);
+                selectedTeams.Add(team);
+
+                WireUpLists();
+            }
         }
     }
 }
