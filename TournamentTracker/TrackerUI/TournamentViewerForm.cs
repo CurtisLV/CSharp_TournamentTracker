520c5575899810774ec048f6e0a9c21e2a643bf8
CurtisLV <curtislv@gmail.com>
2022-10-28 19:42:34 +0300

    TournamentViewer form 2

--- TournamentTracker/TrackerUI/TournamentViewerForm.cs
+++ TournamentTracker/TrackerUI/TournamentViewerForm.cs
@@ -5,6 +5,7 @@ namespace TrackerUI
     public partial class TournamentViewerForm : Form
     {
         private TournamentModel tournament;
+        List<int> rounds = new List<int>();
 
         public TournamentViewerForm(TournamentModel tournamentModel)
         {
@@ -17,6 +18,22 @@ namespace TrackerUI
         private void LoadFormData()
         {
             tournamentName.Text = tournament.TournamentName;
+        }
+
+        private void LoadRounds()
+        {
+            rounds.Add(1);
+            int currRound = 1;
+
+            foreach (List<MatchupModel> matchups in tournament.Rounds)
+            {
+                if (matchups.First().MatchupRound > currRound)
+                {
+                    currRound = matchups.First().MatchupRound;
+                    rounds.Add(currRound);
+                    
+                }
+            }
         }
     }
 }
