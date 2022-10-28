72c8f6d3e1532d724be203e179893729d57bfde4
CurtisLV <curtislv@gmail.com>
2022-10-28 19:33:59 +0300

    Link up Load Tournament button

--- TournamentTracker/TrackerUI/TournamentDashbordForm.Designer.cs
+++ TournamentTracker/TrackerUI/TournamentDashbordForm.Designer.cs
@@ -77,6 +77,7 @@
             this.loadTournamentButton.TabIndex = 34;
             this.loadTournamentButton.Text = "Load Tournament";
             this.loadTournamentButton.UseVisualStyleBackColor = true;
+            this.loadTournamentButton.Click += new System.EventHandler(this.loadTournamentButton_Click);
             // 
             // createTournamentButton
             // 
