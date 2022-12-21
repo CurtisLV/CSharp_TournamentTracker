1b0382a7dea1371388ee8cea76e30e9faa61dc83
CurtisLV <curtislv@gmail.com>
2022-12-21 20:35:25 +0200

    Cleanup 2

--- TournamentTracker/TournamentTracker/Models/PrizeModel.cs
+++ TournamentTracker/TournamentTracker/Models/PrizeModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrackerLibrary.Models;
+﻿namespace TrackerLibrary.Models;
 
 public class PrizeModel
 {
